34c772d74c93f3eff20390f3fa380fa927e5adc0
Mauricio Bedoya <bedoyama@gmail.com>
2026-06-26 21:35:17 -0500

    play with signals

--- practice/03. fun-with-signals/src/app/app.component.ts
+++ practice/03. fun-with-signals/src/app/app.component.ts
@@ -1,38 +1,44 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
   // 8. Set change detection strategy to OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   // 1. replace with a writeable signal with an initial value of 0
-  readonly firstNumber = undefined; 
+  readonly firstNumber = signal(2); 
 
   // 2. replace with a writeable signal with an initial value of 0
-  readonly secondNumber = undefined;
+  readonly secondNumber = signal(5);
 
   // 3. replace with a computed signal that emits the sum of the first and second numbers
-  readonly sum = undefined;
+  readonly sum = computed(() => this.firstNumber() + this.secondNumber());
 
   setSecondSignalTo10() {
     // 4. set the second number signal to 10
+    this.secondNumber.set(10);
   }
 
   incrementFirstSignal() {
     // 5. increment the first number signal by 1 but only if it's less than 10
+    this.firstNumber.update((value) => (value < 10 ? value + 1 : value));
   }
 
   incrementBothSignals() {
     // 6. increment both number signals by 1 with a maximum of 10
+    this.firstNumber.update((value) => (value < 10 ? value + 1 : value));
+    this.secondNumber.update((value) => (value < 10 ? value + 1 : value));
   }
 
 
   constructor() {
     // 7. Define an effect that displays both signals to the console whenever any of them changes
+    console.log('Effect: firstNumber =', this.firstNumber(), ', secondNumber =', this.secondNumber());
   }
 }
