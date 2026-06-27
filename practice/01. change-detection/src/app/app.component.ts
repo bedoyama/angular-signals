eb8f206e697cc8b07f4bc994bc6e558d1a76cb9b
Mauricio Bedoya <bedoyama@gmail.com>
2026-06-26 20:49:08 -0500

    behavior subject exercise

--- practice/01. change-detection/src/app/app.component.ts
+++ practice/01. change-detection/src/app/app.component.ts
@@ -6,17 +6,22 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject } from '@
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   // 1. add change detection strategy here
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.Default,
 })
 export class AppComponent {
   counter = 0;
 
   // 2. inject ChangeDetectorRef here
-  readonly changeDetector = inject(ChangeDetectorRef);
+  // readonly changeDetector = inject(ChangeDetectorRef);
 
   doNothing() {
     // This method intentionally left blank
   }
+
+  calculatedValue() {
+    console.log('calculatedValue() called');
+    return 42;
+  } 
 
   constructor() {
     setInterval(() => {
@@ -25,10 +30,10 @@ export class AppComponent {
     }, 1000);
 
     // 3. Add another interval that calls detectChanges() every 5 seconds
-    setInterval(() => {
-      this.changeDetector.detectChanges();
-      console.log('Change detection triggered');
-    }, 5000);
+    // setInterval(() => {
+    //   this.changeDetector.detectChanges();
+    //   console.log('Change detection triggered');
+    // }, 5000);
   }
 
 }
