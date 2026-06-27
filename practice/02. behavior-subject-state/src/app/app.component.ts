8d0d6f7b012b840e5d95f97d5535b65ed702b401
Mauricio Bedoya <bedoyama@gmail.com>
2026-06-26 21:11:00 -0500

    behavior subject

--- practice/02. behavior-subject-state/src/app/app.component.ts
+++ practice/02. behavior-subject-state/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { interval } from 'rxjs';
+import { BehaviorSubject, combineLatest, debounceTime, firstValueFrom, interval, map } from 'rxjs';
+
+type Options = Record<string, string>;
 
 @Component({
   selector: 'app-root',
@@ -9,24 +11,58 @@ import { interval } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   // 1. set change detection strategy to OnPush
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  readonly counter$ = interval(1000);
+  readonly a$ = new BehaviorSubject<number>(1);
+  readonly b$ = new BehaviorSubject<number>(2);
+  readonly sum$ = combineLatest([this.a$, this.b$]).pipe(map(([a, b]) => a + b));
 
+  async incA() {
+    // only increment A if A + B is less than 10
 
-  calculatedValue() {
-    console.log('calculatedValue() called');
-    return 42;
+    const sum = await firstValueFrom(this.sum$);
+    // Issues with the above code:
+    // 1. It will only get the current value of sum$ at the time of the call, and will not react to future changes.
+    // 2. It will not trigger change detection if the value of sum$ changes after the call.
+    if (sum < 10) {
+      this.a$.next(this.a$.value + 1);
+    }
   }
-  // 2. Remove the counter property and the constructor
-  // counter = 0;
+
+  readonly options$ = new BehaviorSubject<Options>({'r': 'Red', 'g': 'Green', 'b': 'Blue'});
+  readonly selectedKey$ = new BehaviorSubject<string>('b');
+
+  readonly selectedValue$ = combineLatest([this.options$, this.selectedKey$]).pipe(
+    debounceTime(0),
+    map(([options, key]) => options[key]), 
+  );
+
+  switchOptions() {
+    this.options$.next({'m': 'Magenta', 'y': 'Yellow', 'c': 'Cyan'});
+    this.selectedKey$.next('c');
+  }
 
   constructor() {
-    // this.counter$.subscribe((value) => {
-    //   this.counter = value;
-    // });
+    this.selectedValue$.subscribe(console.log);
   }
+
+
+  // readonly counter$ = interval(1000);
+
+
+  // calculatedValue() {
+  //   console.log('calculatedValue() called');
+  //   return 42;
+  // }
+  // // 2. Remove the counter property and the constructor
+  // // counter = 0;
+
+  // constructor() {
+  //   // this.counter$.subscribe((value) => {
+  //   //   this.counter = value;
+  //   // });
+  // }
 
   // 3. In the html, bind directly to the counter$ observable using the async pipe
 }
